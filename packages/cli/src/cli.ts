bbf7075f943a351f67e99daf4dc3100eb06da496
Joakim Carlstein <joakim@aboviq.com>
2024-01-18 10:40:53 +0100

    fix(cli): remove --enable-source-maps flag

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node --enable-source-maps
+#!/usr/bin/env node
 import process from 'node:process';
 import { parseArgs } from 'node:util';
 import importFromEsm from 'import-from-esm';
